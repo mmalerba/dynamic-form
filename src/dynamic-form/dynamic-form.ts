3c83069a7edabfd4b98c768fb703d29a0323d9cc
Miles Malerba <mmalerba@google.com>
2025-10-24 16:26:18 -0700

    don't reset values the user has changed from the initial

--- src/dynamic-form/dynamic-form.ts
+++ src/dynamic-form/dynamic-form.ts
@@ -31,14 +31,22 @@ export interface FieldSpec {
 export class DynamicForm {
   spec = input.required<FieldSpec[]>();
 
-  model = linkedSignal({
+  model = linkedSignal<FieldSpec[], string[]>({
     source: this.spec,
-    computation: (src) => src.map((it) => it.initial),
+    computation: (src, prev) =>
+      src.map((it) => {
+        // If the value hasn't changed from the initial, update to the new initial,
+        // otherwise keep the old value.
+        const prevIndex = prev?.source.findIndex((spec) => it.name === spec.name) ?? -1;
+        return prevIndex > -1 && prev?.value[prevIndex] !== prev?.source[prevIndex].initial
+          ? prev?.value[prevIndex]!
+          : it.initial;
+      }),
   });
 
   form = form(this.model, (p) => {
     applyEach(p, (it) => {
-      required(it, { when: ({ index }) => !!this.spec()[index()].validation?.required });
+      required(it, { when: ({ index }) => !!this.spec()[index()].validation.required });
     });
   });
 }
